f96e40c1410807781a6ca8edff990135992cd7a7
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-09-03 15:36:07 -0400

    Add files via upload

---
+++ Basic/BasicExercise15/BasicExercise15/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+//  Author: Jonathan Scholl
+//  Date: 2/26/2021
+//  Exercise: https://www.w3resource.com/csharp-exercises/basic/index.php exercise 15
+//  Description:  Write a C# program remove specified a character from a non-empty string 
+//  using index of a character.
+//
+//  Comment: I just looked at the answer for this one, because I'm not great with string
+//  manipulation and I don't even remember learning the function used in the exercise.
+//  I found a different way to use what I learned to output the same answer.
+*/
+using System;
+using System.Collections.Generic;
+
+namespace BasicExercise15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "w3resource";
+            
+            //Don't remember learning the Remove function, but I understand how it works after this after playing around.
+            //first number is position in the string and second is how many characters removed after position
+            Console.WriteLine(str.Remove(1, 1));
+            Console.WriteLine(str.Remove(9, 1));
+            Console.WriteLine(str.Remove(0, 1));
+        }
+
+    }
+}
+
+/*
+ * the website's answer:
+ * public class Exercise15 {
+  static void Main(string[] args)
+        {
+            Console.WriteLine(remove_char("w3resource", 1));
+            Console.WriteLine(remove_char("w3resource", 9));
+            Console.WriteLine(remove_char("w3resource", 0));     
+        }
+       public static string remove_char(string str, int n)
+        {
+            return str.Remove(n, 1);
+        }
+}
+ */
---
+++ Basic/BasicExercise16/BasicExercise16/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+//  Author: Jonathan Scholl
+//  Date: 2/26/2021
+//  Exercise: https://www.w3resource.com/csharp-exercises/basic/index.php exercise 16
+//  Description: Write a C# program to create a new string from a given string where 
+//  the first and last characters will change their positions
+//  Comment: Not great with string manipulating, so I'm just studying answers
+*/
+using System;
+
+namespace BasicExercise16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(first_last("w3resource"));
+            Console.WriteLine(first_last("Python"));
+            Console.WriteLine(first_last("x"));
+        }
+
+        public static string first_last(string str)
+        {
+            //checks to see if string is longer than 1 letter
+            return str.Length > 1
+                ? str.Substring(str.Length - 1)             //last letter in string
+                + str.Substring(1, str.Length - 2)          //everything in between first and last letters
+                + str.Substring(0, 1) : str;                //first letter in string
+
+            //basically breaks it down into pieces and adds it back together in different order to produce
+            //desired output
+        }
+    }
+}
