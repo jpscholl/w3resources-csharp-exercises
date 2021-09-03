2dcd680630754dc9a665a0d99d8e92dbbc1487e3
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-09-03 15:27:41 -0400

    Add files via upload

---
+++ Basic/BasicExercise1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+//  Author: Jonathan Scholl
+//  Date: 2/17/2021
+//  https://www.w3resource.com/csharp-exercises/basic/index.php
+*/
+using System;
+
+namespace BasicExercise1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //2 ways to do it...one is separate console.writelines or use \n breakline like down below
+            Console.WriteLine("Hello World!\nJonathan Scholl");    
+        }
+    }
+}
