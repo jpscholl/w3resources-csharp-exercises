423f606236df31b05f76ddd8801b62863195d5e3
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-09-03 16:04:57 -0400

    Add files via upload

---
+++ Basic/BasicExercise91/BasicExercise91/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+//  Author: Jonathan Scholl
+//  Date: 7/12/2021
+//  Exercise: https://www.w3resource.com/csharp-exercises/basic/index.php exercise 91
+//  Description: Write a C# Sharp program to remove all the values except integer values from a given array of mixed values.
+*/
+using System;
+using System.Linq;
+
+namespace BasicExercise91
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] mixedArray = new object[6];
+            mixedArray[0] = 25;
+            mixedArray[1] = "Anna";
+            mixedArray[2] = false;
+            mixedArray[3] = System.DateTime.Now;
+            mixedArray[4] = -112;
+            mixedArray[5] = -34.67;
+            Console.WriteLine("Original array elements:");
+            for (int i = 0; i < mixedArray.Length; i++)
+            {
+                Console.Write(mixedArray[i] + " ");
+            }
+            int[] new_nums = RemoveNonIntegers(mixedArray);
+            Console.WriteLine("\n\nAfter removing all the values except integer values from the said array of mixed values:");
+            for (int i = 0; i < new_nums.Length; i++)
+            {
+                Console.Write(new_nums[i] + " ");
+            }
+        }
+        public static int[] RemoveNonIntegers(object[] nums)
+        {
+            return nums.OfType<int>().ToArray();
+        }
+    }
+}
---
+++ Basic/BasicExercise92/BasicExercise92/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+//  Author: Jonathan Scholl
+//  Date: 7/12/2021
+//  Exercise: https://www.w3resource.com/csharp-exercises/basic/index.php exercise 92
+//  Description: Write a C# Sharp program to find the next prime number of a given number. If the given number is a prime number, return the number
+*/
+using System;
+
+namespace BasicExercise92
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a number: ");
+            int numInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Original number: " + numInput);
+            Console.WriteLine("Next prime number/Current prime number: " + FindPrimeNumber(numInput));
+        }
+
+        public static int FindPrimeNumber(int num)
+        {
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0) { num++; i = 2; }
+            }
+            return num;
+        }
+    }
+}
