0bad1c52acd3f62ad6b697585dbebdacc0ed2ccd
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-09-03 15:17:35 -0400

    Add files via upload

---
+++ BasicAlgorithm/BasicAlgorithm1/BasicAlgorithm2/BasicAlgorithm2/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+//  Author: Jonathan Scholl
+//  Date: 7/14/2021
+//  Exercise: https://www.w3resource.com/csharp-exercises/basic-algo/index.php exercise 2
+//  Description: Write a C# Sharp program to get the absolute difference between n and 51. If n is greater than 51 return triple the absolute difference
+*/
+using System;
+
+namespace BasicAlgorithm2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask user for input then store number into integer variable
+            Console.WriteLine("Please input a number: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            //outputs the results after userNumber is passed into the AbsolTriple51 method
+            Console.WriteLine("The results after calculation are: " + AbsolTriple51(userNumber));
+        }
+        //method for calculation
+        public static int AbsolTriple51(int input)
+        {
+            //since we know the number 51 is constant
+            const int x = 51;
+
+            //beginning of if statement (condidtion checks if input is greater than x)
+            if (input > x)
+            {
+                //if the userinput is greater than 51 then it'll output the difference times 3
+                return (input - x) * 3;
+            }
+            //else it'll return just the difference
+            return x - input;
+        }
+    }
+}
---
+++ BasicAlgorithm/BasicAlgorithm1/BasicAlgorithm3/BasicAlgorithm3/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+//  Author: Jonathan Scholl
+//  Date: 7/14/2021
+//  Exercise: https://www.w3resource.com/csharp-exercises/basic-algo/index.php exercise 3
+//  Description: Write a C# Sharp program to check two given integers, and return true if one of them is 30 or if their sum is 30.
+*/
+using System;
+
+namespace BasicAlgorithm3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //prompt user for input and store into int variable
+            Console.WriteLine("Please input a number: ");
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
+
+            //prompt user for input and store into int variable
+            Console.WriteLine("Please input a number: ");
+            int userNum2 = Convert.ToInt32(Console.ReadLine());
+
+            //output results after passing userInput to method for calculation
+            Console.WriteLine("One of the numbers or the sum of the numbers is 30: " + checkSum30(userNum1, userNum2));
+        }
+
+        //boolean variable for testing. Takes two ints as reference
+        public static bool checkSum30(int x, int y)
+        {
+            //condition: if x equivalent to 30 OR y equivalent to 30 OR x plus y is equivalent to 30
+            if (x == 30 || y == 30 || x + y == 30)
+            {
+                //if above conditions are met, returns true
+                return true;
+            }
+            else
+            {
+                //if conditions aren't met, return false
+                return false; 
+            }        
+        }
+    }
+}
