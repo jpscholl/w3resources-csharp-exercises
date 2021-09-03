b95011ff0fd4624af07edc1159728afee1ea0521
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-09-03 15:40:48 -0400

    Add files via upload

---
+++ Basic/BasicExercise29/BasicExercise29/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+//  Author: Jonathan Scholl
+//  Date: 3/3/2021
+//  Exercise: https://www.w3resource.com/csharp-exercises/basic/index.php exercise 29
+//  Description: Write a C# program to find the size of a specified file in bytes.
+//  Comment: Not too familiar with FileInfo, so I decided to explore more in depth
+//  Many interesting and useful properties available.
+*/
+using System;
+using System.IO;
+
+namespace BasicExercise29
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter file path: ");
+            var filePath = Console.ReadLine();
+
+            FileInfo file = new FileInfo(filePath);
+            Console.WriteLine("Selected file: " + file.Name);
+            Console.WriteLine("Size of a file: " + file.Length.ToString() + " bytes");
+            Console.WriteLine("Last accesssed: " + file.LastAccessTime);
+            Console.WriteLine("Last write time: " + file.LastWriteTime);
+            Console.WriteLine("Creation date: " + file.CreationTime);
+            Console.WriteLine("Does this file exist: " + file.Exists);
+
+        }
+    }
+}
---
+++ Basic/BasicExercise30/BasicExercise30/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+//  Author: Jonathan Scholl
+//  Date: 3/3/2021
+//  Exercise: https://www.w3resource.com/csharp-exercises/basic/index.php exercise 30
+//  Description: Write a C# program to convert a hexadecimal number to decimal number.
+*/
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+public class Exercise30
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter hexadecimal number:");
+        string hexNum = Console.ReadLine();
+
+        Console.WriteLine("Hexadecimal number: " + hexNum);
+        int decValue = int.Parse(hexNum, NumberStyles.HexNumber);
+        Console.WriteLine("Converted to-");
+        Console.WriteLine("Decimal number: " + decValue);
+
+    }
+}
