05415251cd87d5d620fdce0c3bda8ac9d490e45e
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-09-03 16:09:37 -0400

    Add files via upload

---
+++ BasicAlgorithms/BasicAlgorithm8/BasicAlgorithm8/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+//  Author: Jonathan Scholl
+//  Date: 7/14/2021
+//  Exercise: https://www.w3resource.com/csharp-exercises/basic-algo/index.php exercise 8
+//  Description:  Write a C# Sharp program to create a new string which is 4 copies of the 2 front characters of a given string. If the given string length is less than 2 return the original string
+*/
+using System;
+
+namespace BasicAlgorithm8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //prompt user to input string and store string into variable
+            Console.WriteLine("Please input string: ");
+            string userInputString = Console.ReadLine();
+
+            //pass user string to method and output results
+            Console.WriteLine(FrontCharCopy(userInputString));
+        }
+        public static string FrontCharCopy(string userInput)
+         {
+            //returns a string that is the first two letters added together four times if the string is greater than 2. Otherwise output original string
+            return userInput.Length > 2 
+                ? userInput.Substring(0, 2) 
+                + userInput.Substring(0, 2)
+                + userInput.Substring(0, 2) 
+                + userInput.Substring(0, 2) : userInput;
+         }
+    }
+}
---
+++ BasicAlgorithms/BasicAlgorithm9/BasicAlgorithm9/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+//  Author: Jonathan Scholl
+//  Date: 7/14/2021
+//  Exercise: https://www.w3resource.com/csharp-exercises/basic-algo/index.php exercise 9
+//  Description: Write a C# Sharp program to create a new string with the last char added at the front and back of a given string of length 1 or more
+*/
+using System;
+
+namespace BasicAlgorithm9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //prompt user to input string and store string into variable
+            Console.WriteLine("Please input string: ");
+            string userInputStr = Console.ReadLine();
+
+            //pass user string to method and output results
+            Console.WriteLine(AddCharFrontAndBack(userInputStr));
+        }
+        //method to add last letter to front and end of string
+        public static string AddCharFrontAndBack(string userInput)
+        {
+            //stores last letter of a string
+            string lastLetter = userInput.Substring(userInput.Length - 1);
+            //returns string based on last letter + original string + last letter
+            return  lastLetter + userInput + lastLetter;
+        }
+    }
+}
