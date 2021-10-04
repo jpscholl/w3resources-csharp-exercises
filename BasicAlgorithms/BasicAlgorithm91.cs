d4bd7f103e80f44dd44913eeb3bee1fa378d6a65
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-10-04 16:44:52 -0400

    Create BasicAlgorithm91.cs
    Simple algorithm. Reused methods to save time.

---
+++ BasicAlgorithms/BasicAlgorithm91.cs
@@ -0,0 +1,52 @@
+/*
+//  Author: Jonathan Scholl
+//  Date: 10/4/2021
+//  Project: https://www.w3resource.com/csharp-exercises/basic-algo/index.php 91
+//  Description: Write a C# Sharp program to compute the sum of the elements of an given array of integers.
+*/
+using System;
+
+namespace BasicAlgorithm91
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many elements: ");
+            var arrayLength = int.Parse(Console.ReadLine());
+
+            int[] userNumArray = storeArrayInput(arrayLength);
+
+            printArray(userNumArray);
+
+            sumOfArray(userNumArray);
+        }
+
+        public static int[] storeArrayInput(int arrLength)
+        {
+            int[] storeInput = new int[arrLength];
+
+            for (int x = 0; x < arrLength; x++)
+            {
+                storeInput[x] = int.Parse(Console.ReadLine());
+            }
+
+            return storeInput;
+        }
+
+        public static void printArray(int[] numArray)
+        {
+            Console.Write("\nContents of the array: ");
+            Console.WriteLine("[{0}]", string.Join(", ", numArray));
+        }
+        public static void sumOfArray(int[] numArray)
+        {
+            int sum = 0;
+            foreach (int item in numArray)
+            {
+                sum += item;
+            }
+            Console.Write("Sum of the array: " + sum);
+        }
+    }
+}
