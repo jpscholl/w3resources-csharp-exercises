ddb6de3265a4c8127f472ca913dd261edff4fe98
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2022-03-28 22:01:04 -0400

    Create String01.cs

---
+++ String/String01.cs
@@ -0,0 +1,10 @@
+/*
+//  Author: Jonathan Scholl
+//  Date: 3/28/2022
+//  Project: https://www.w3resource.com/csharp-exercises/string/index.php
+//  String1.cs
+*/
+
+Console.Write("Please input a string: ");
+string userInput = Console.ReadLine();
+Console.WriteLine($"The string you input was: {userInput}");
